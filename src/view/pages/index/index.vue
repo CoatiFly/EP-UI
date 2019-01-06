6143fbc05e54e8a1a6bba8b3ab54630daee041e1
Yocrab <dugucanfeng@163.com>
2019-01-06 22:27:19 +0800

    add docs

--- src/view/pages/index/index.vue
+++ src/view/pages/index/index.vue
@@ -3,12 +3,13 @@
     <div class="timeline_box">
       <ul class="list_box">
         <li class="item" v-for="(item, index) in listData" :key="index">
-          <div class="style_pos" :class="{redbg: item.isRed}" :style="{width: item.width, bottom: item.bottom, left: item.left, animationDelay: item.delay, zIndex: index+1}">
+          <!-- prettier-ignore -->
+          <div class="style_pos" :class="{ redbg: item.isRed }" :style="{ width: item.width, bottom: item.bottom, left: item.left, animationDelay: item.delay, zIndex: index+1 }">
             <div class="text">
-              <div class="title">{{item.time}}</div>
-              <div class="detail">{{item.detail}}</div>
+              <div class="title">{{ item.time }}</div>
+              <div class="detail">{{ item.detail }}</div>
             </div>
-            <div class="line" :style="{height: item.line}"></div>
+            <div class="line" :style="{ height: item.line }"></div>
             <div class="point"></div>
           </div>
         </li>
